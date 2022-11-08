688a09e84702069cf5c7971471a0f2dfcfb11887
Ivavsys <Ivavsys@192.168.1.55>
2022-11-08 11:19:57 +0530

    driver commit

--- src/test/java/StepDefination/TestRunner.java
+++ src/test/java/StepDefination/TestRunner.java
@@ -7,11 +7,9 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/featureFiles",glue={"StepDefination"},
-monochrome=true,
-plugin={"pretty","html:target/HTMLReports"})
-//plugin={"pretty","html:target/JSONReports/JSONReports"})
-//plugin={"pretty","html:target/JUNITeports/JUNITReports"})
-
+monochrome = true, dryRun = false, tags = "@RadioTest", strict = true, plugin = {
+		/*"html:target/HTMLRepsort"*/ "json:target/BDD-Report/cucumber.json", "pretty", "html:target/BDD-Report/cucumber.html","com.cucumber.listener.ExtentCucumberFormatter:target/BDD-Report/HTML-DashboardReport.html"}
+)
 public class TestRunner {
 
 }
